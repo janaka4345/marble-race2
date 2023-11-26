5eb4a663d39ac244b78f1e977b76988af90540b1
CodeSandbox <support@codesandbox.io>
2023-11-26 02:02:01 +0000

    Sandbox snapshot commit

---
+++ src/components/MarbleRace.jsx
@@ -0,0 +1,12 @@
+export default function MarbleRace() {
+  return (
+    <Canvas shadows={true}
+    camera={{
+      fov: 40,
+      near: 0.2,
+      far: 200,
+      position: [15, 15, 15]>
+
+    </Canvas>
+  );
+}
